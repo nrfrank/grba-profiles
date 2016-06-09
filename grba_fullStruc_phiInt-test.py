afa6eaf10a76decf09a7cc4d06fcf5dc103b662d
nrfrank <nrfrank@live.unc.edu>
2016-06-09 17:13:11 -0400

    Integrand plotting
    Generated some test plots of the integrated in phi space. Now looking at
    the plots of the integrand against phi for various values of y. Trying
    to figure out labeling.

--- grba_fullStruc_phiInt-test.py
+++ grba_fullStruc_phiInt-test.py
@@ -2,7 +2,8 @@ import numpy as np
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 import seaborn as sns
-from grba_profiles import intG, thetaPrime, fluxG_fullStr, r0_max
+from grba_profiles import intG, thetaPrime, r0_max
+from time import time
 
 def rootFuncR(R, R0, kap, sig, thv, phi):
     thP = thetaPrime(R, thv, phi)
@@ -13,41 +14,57 @@ def rootFuncR(R, R0, kap, sig, thv, phi):
     right = (np.tan(thv) + R0)**2.0*np.exp2(rExp)
     return(left - right)
 
+def fluxG_fullStr(r, y, kap, sig, thv, gA = 1.0, k = 0.0, p = 2.2):
+    Gk = (4.0 - k)*gA**2.0
+    thP0 = thetaPrime(r, thv, 0.0)
+    exp0 = np.power(np.divide(thP0, sig), 2.0*kap)
+    chiVal = np.divide(y - Gk*np.exp2(-exp0)*(np.tan(thv) + r)**2.0, np.power(y, 5.0 - k))
+    return r*intG(y, chiVal)
+
 N = 100
-KAPPA = 1.0
+TINY = np.power(10.0, -3.0)
+KAPPA = 0.0
 SIGMA = 2.0
-THETA_V = np.radians(0.0)
-Y_VAL = 0.7
+THETA_V = np.radians(6.0)
+Y_VAL = TINY
 R_MAX = r0_max(Y_VAL, KAPPA, SIGMA, THETA_V, gA = 1.0, k = 0.0, p = 2.2)
-
-print R_MAX
-
-rVals = np.linspace(0.0, R_MAX, N)
-pVals = np.linspace(0.0, 2.0*np.pi, N)
-P, R = np.meshgrid(pVals, rVals)
-RP = np.zeros((N, N))
-
-start = time.time()
-for i in range(N):
-    guess = R[i][0]
-    for j in range(N):
-        rP = fsolve(rootFuncR, guess, args = (R[i][j], KAPPA, SIGMA, THETA_V, P[i][j]))
-        # print R[i][j], P[i][j], rP
-        if rP == 0.0 and R[i][0] == 0.0:
-            RP[i][j] = 1.0
-        else:
-            RP[i][j] = (rP/R[i][j])**2.0
-        guess = rP
-
-stop = time.time()
-runtime = stop - start
-print "runtime = ", runtime, ", final R' value = ", rP
-
 plt.figure()
+for y in [TINY, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0]:
+    Y_VAL = y
+    R_MAX = r0_max(Y_VAL, KAPPA, SIGMA, THETA_V, gA = 1.0, k = 0.0, p = 2.2)
+    rVals = np.linspace(0.0, R_MAX, N)
+    pVals = np.linspace(0.0, 2.0*np.pi, N)
+    P, R = np.meshgrid(pVals, rVals)
+    RP = np.zeros((N, N))
+    
+    start = time()
+    for i in range(N):
+        flux = []
+        guess = R[i][0]
+        for j in range(N):
+            rP = fsolve(rootFuncR, guess, args = (R[i][j], KAPPA, SIGMA, THETA_V, P[i][j]))
+            # print R[i][j], P[i][j], rP
+            if rP == 0.0 and R[i][0] == 0.0:
+                f = 1.0
+            else:
+                f = (rP/R[i][j])**2.0
+            guess = rP
+            RP[i][j] = f
+            flux.append(fluxG_fullStr(R[i][j], Y_VAL, KAPPA, SIGMA, THETA_V)*f)
+        flux = [f/max(flux) for f in flux]
+        plt.plot(pVals, flux) 
+
+    stop = time()
+    runtime = stop - start
+    print "runtime = ", runtime, ", final R' value = ", rP
+    plt.axis([min(pVals), max(pVals), min(flux), max(flux)])
+plt.show()
+
+# plt.figure()
 # RP = ma.masked_invalid(RP)
-plt.pcolormesh(P, R, RP, cmap = plt.cm.viridis)
-plt.colorbar()
+# plt.pcolormesh(P, R, RP, cmap = plt.cm.viridis)
+# plt.colorbar()
 # plt.axhline(y = np.pi/2.0, color = 'r')
 # plt.axhline(y = np.pi*3.0/2.0, color = 'r')
-plt.axis([min(pVals), max(pVals), min(rVals), max(rVals)])
-plt.show()
+# plt.axis([min(pVals), max(pVals), min(rVals), max(rVals)])
+# plt.show()
