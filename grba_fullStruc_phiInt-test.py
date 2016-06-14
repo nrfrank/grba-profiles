87d511907389ba0f872890a3e9e5526329de0e86
nrfrank <nrfrank@live.unc.edu>
2016-06-14 17:11:30 -0400

    Integrand exploratory visualization; r' root solve code.
    Added a function to compute and display surface plot in r0'-y space of
    the integrand or f(phi) function. Appears periodic and well behaved.
    Implemented structure for r' root solve equation and newton-raphson root
    solver and performed some initial test. Appears to be working. Need to
    test against scipy fsolve results.

--- grba_fullStruc_phiInt-test.py
+++ grba_fullStruc_phiInt-test.py
@@ -1,6 +1,10 @@
+import sys
 import numpy as np
+import numpy.ma as ma
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
+import matplotlib.colors as c
+from mpl_toolkits.mplot3d import Axes3D
 import seaborn as sns
 from grba_profiles import intG, thetaPrime, r0_max
 from time import time
@@ -8,7 +12,7 @@ from time import time
 def rootFuncR(R, R0, kap, sig, thv, phi):
     thP = thetaPrime(R, thv, phi)
     lExp = -np.power(np.divide(thP, sig), 2.0*kap)
-    left = (np.tan(thv)**2.0 + 2.0*R*np.tan(thv)*np.abs(np.cos(phi)) + R**2.0)*np.exp2(lExp)
+    left = (np.tan(thv)**2.0 + 2.0*R*np.tan(thv)*np.cos(phi) + R**2.0)*np.exp2(lExp)
     thP0 = thetaPrime(R0, thv, 0.0)
     rExp = -np.power(np.divide(thP0, sig), 2.0*kap)
     right = (np.tan(thv) + R0)**2.0*np.exp2(rExp)
@@ -21,50 +25,83 @@ def fluxG_fullStr(r, y, kap, sig, thv, gA = 1.0, k = 0.0, p = 2.2):
     chiVal = np.divide(y - Gk*np.exp2(-exp0)*(np.tan(thv) + r)**2.0, np.power(y, 5.0 - k))
     return r*intG(y, chiVal)
 
-N = 100
-TINY = np.power(10.0, -3.0)
-KAPPA = 0.0
-SIGMA = 2.0
-THETA_V = np.radians(6.0)
-Y_VAL = TINY
-R_MAX = r0_max(Y_VAL, KAPPA, SIGMA, THETA_V, gA = 1.0, k = 0.0, p = 2.2)
-plt.figure()
-for y in [TINY, 0.1, 0.3, 0.5, 0.7, 0.9, 1.0]:
-    Y_VAL = y
-    R_MAX = r0_max(Y_VAL, KAPPA, SIGMA, THETA_V, gA = 1.0, k = 0.0, p = 2.2)
-    rVals = np.linspace(0.0, R_MAX, N)
-    pVals = np.linspace(0.0, 2.0*np.pi, N)
+def surfacePlot_r0Phi(num, yVal, kap, sig, thv, gA = 1.0, k = 0.0, p = 2.2):
+    R0_MAX = r0_max(yVal, kap, sig, thv, gA, k, p)
+    rVals = np.linspace(0.0, R0_MAX, num)
+    pVals = np.linspace(0.0, 2.0*np.pi, num)
     P, R = np.meshgrid(pVals, rVals)
-    RP = np.zeros((N, N))
-    
-    start = time()
-    for i in range(N):
-        flux = []
+    F_ = np.zeros((num, num))
+    for i in range(num):
+        # flux = []
         guess = R[i][0]
-        for j in range(N):
-            rP = fsolve(rootFuncR, guess, args = (R[i][j], KAPPA, SIGMA, THETA_V, P[i][j]))
+        for j in range(num):
+            rP = fsolve(rootFuncR, guess, args = (R[i][j], kap, sig, thv, P[i][j]))
             # print R[i][j], P[i][j], rP
             if rP == 0.0 and R[i][0] == 0.0:
                 f = 1.0
             else:
                 f = (rP/R[i][j])**2.0
             guess = rP
-            RP[i][j] = f
-            flux.append(fluxG_fullStr(R[i][j], Y_VAL, KAPPA, SIGMA, THETA_V)*f)
-        flux = [f/max(flux) for f in flux]
-        plt.plot(pVals, flux) 
+            F_[i][j] = f*fluxG_fullStr(R[i][j], yVal, kap, sig, thv, gA, k, p)
+            # flux.append(fluxG_fullStr(R[i][j], yVal, kap, sig, thv)*f)
+        # flux = [f/max(flux) for f in flux]
+    
+    plt.figure()
+    F_ = ma.masked_invalid(F_)
+    plt.pcolormesh(P, R, F_, cmap = plt.cm.viridis, norm = c.LogNorm())
+    plt.colorbar()
+    # plt.axhline(y = np.pi/2.0, color = 'r')
+    # plt.axhline(y = np.pi*3.0/2.0, color = 'r')
+    plt.plot([np.pi, np.pi], [0.0, R0_MAX], 'r-')
+    plt.axis([min(pVals), max(pVals), min(rVals), max(rVals)])
+    plt.show()
+    plt.clf()
+    plt.close()
+    
+    return F_
+    
+def surfacePlot_r0Phi_3D(num, yVal, kap, sig, thv, gA = 1.0, k = 0.0, p = 2.2):
+    R0_MAX = r0_max(yVal, kap, sig, thv, gA, k, p)
+    rVals = np.linspace(0.0, R0_MAX, num)
+    pVals = np.linspace(0.0, 2.0*np.pi, num)
+    P, R = np.meshgrid(pVals, rVals)
+    F_ = np.zeros((num, num))
+    for i in range(num):
+        # flux = []
+        guess = R[i][0]
+        for j in range(num):
+            rP = fsolve(rootFuncR, guess, args = (R[i][j], kap, sig, thv, P[i][j]))
+            # print R[i][j], P[i][j], rP
+            if rP == 0.0 and R[i][0] == 0.0:
+                f = 1.0
+            else:
+                f = (rP/R[i][j])**2.0
+            guess = rP
+            F_[i][j] = f*fluxG_fullStr(R[i][j], yVal, kap, sig, thv, gA, k, p)
+            # flux.append(fluxG_fullStr(R[i][j], yVal, kap, sig, thv)*f)
+        # flux = [f/max(flux) for f in flux]
+    
+    fig = plt.figure()
+    ax = fig.gca(projection = '3d')
+    F_ = ma.masked_invalid(F_)
+    surf = ax.plot_surface(P, R, F_, cmap = plt.cm.viridis, norm = c.LogNorm())
+    fig.colorbar(surf)
+    plt.show()
+    plt.clf()
+    plt.close()
+    
+    return F_
 
-    stop = time()
-    runtime = stop - start
-    print "runtime = ", runtime, ", final R' value = ", rP
-    plt.axis([min(pVals), max(pVals), min(flux), max(flux)])
-plt.show()
+def main():
+    N = 100
+    TINY = np.power(10.0, -3.0)
+    KAPPA = 1.0
+    SIGMA = 2.0
+    THETA_V = np.radians(6.0)
+    Y_VAL = 0.25
+    R_MAX = r0_max(Y_VAL, KAPPA, SIGMA, THETA_V, gA = 1.0, k = 0.0, p = 2.2)
+    # F_PHI = surfacePlot_r0Phi(N, Y_VAL, KAPPA, SIGMA, THETA_V)
+    F_PHI = surfacePlot_r0Phi_3D(N, Y_VAL, KAPPA, SIGMA, THETA_V)
 
-# plt.figure()
-# RP = ma.masked_invalid(RP)
-# plt.pcolormesh(P, R, RP, cmap = plt.cm.viridis)
-# plt.colorbar()
-# plt.axhline(y = np.pi/2.0, color = 'r')
-# plt.axhline(y = np.pi*3.0/2.0, color = 'r')
-# plt.axis([min(pVals), max(pVals), min(rVals), max(rVals)])
-# plt.show()
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
--- grba_profile_integration/grba_profile_integration/grba_profile_integration.cpp
+++ grba_profile_integration/grba_profile_integration/grba_profile_integration.cpp
@@ -1,15 +1,23 @@
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+#include <stdio.h>
 #include "nr3.h"
 #include "interp_1d.h"
 #include "quadrature.h"
 #include "romberg.h"
-#include <iostream>
-#include <math.h>
 
 using namespace std;
 
-double integrand(double x)
+const Doub TORAD = M_PI / 180.0;
+
+float integrand(float x)
 {
-	double val;
+	float val;
 	val = pow(x, 4)*log(x + sqrt(pow(x, 2) + 1));
 	return val;
 }
@@ -21,10 +29,67 @@ int addition(int a, int b)
 	return r;
 }
 
-int main()
-{
-	double result;
-	result = qromb(integrand, 0, 2);
+Doub thetaPrime(const Doub r, const Doub thv, const Doub sig, const Doub phi) {
+	Doub numer = r*pow(pow(cos(thv), 2) - 0.25*pow(sin(2.0*thv), 2)*pow(cos(phi), 2), 0.5);
+	Doub denom = 1.0 + 0.5*r*sin(2.0*thv)*cos(phi);
+	return numer / denom;
+}
 
-	cout << "The result is " << result;
+Doub energyProfile(const Doub thp, const Doub sig, const Doub kap) {
+	return exp2(-pow(thp / sig, 2.0*kap));
+}
+
+struct RootFunc
+{
+	Doub r0, kap, thv, sig, phi;
+	RootFunc(const Doub R0, const Doub KAP, const Doub THV, const Doub SIG, const Doub PHI) : r0(R0), kap(KAP), thv(THV*TORAD), sig(SIG), phi(PHI*TORAD) {}
+	Doub f(const Doub r) {
+		const Doub thp = thetaPrime(r, thv, sig, phi);
+		const Doub eng = energyProfile(thp, sig, kap);
+		const Doub lhs = (pow(r, 2) + 2.0*r*tan(thv)*cos(phi) + pow(tan(thv), 2))*eng;
+		const Doub thp0 = thetaPrime(r, thv, sig, 0.0);
+		const Doub eng0 = energyProfile(thp0, sig, kap);
+		const Doub rhs = pow(r0 + tan(thv), 2)*eng0;
+		return lhs - rhs;
+	}
+	Doub df(const Doub r) {
+		const Doub thp = thetaPrime(r, thv, sig, phi);
+		const Doub first = r + tan(thv)*cos(phi);
+		const Doub second = pow(r, 2) + 2 * r*tan(thv)*cos(phi) + pow(tan(thv), 2);
+		const Doub frac = (kap*log(2.0)*pow(thp / sig, 2.0*kap)) / (r*(1.0 + 0.5*r*sin(2.0*thv)*cos(phi)));
+		const Doub exponent = 2.0*energyProfile(thp, sig, kap);
+		return (first - second*frac)*exponent;
+	}
+};
+
+template <class T>
+Doub rtnewt(T &RootFunc, const Doub g, const Doub xacc) {
+	const Int JMAX = 20;
+	Doub rtn = g;
+	for (Int j = 0; j < JMAX; j++) {
+		Doub f = RootFunc.f(rtn);
+		Doub df = RootFunc.df(rtn);
+		Doub dx = f / df;
+		//cout << f << ", " << df << ", " << dx;
+		rtn -= dx;
+		if (abs(dx) < xacc) {
+			cout << "Convergance in " << j << " steps.";
+			return rtn;
+		}
+	}
+	throw("Maximum number of iterations exceeded in rtnewt");
+}
+
+int main(void)
+{
+	/*
+	RootFunc fx(0.1, 0.0, 0.0, 2.0, 0.0);
+	cout << "function at 0.1: " << fx(0.1) << ", derivative at 0.1: " << fx.df(0.1);*/
+	Doub R0, KAPPA, THETA_V, SIGMA, PHI, G;
+	cout << "Enter values for r0, kappa, thetaV, sigma, phi, and initial r guess.";
+	cin >> R0 >> KAPPA >> THETA_V >> SIGMA >> PHI >> G;
+	RootFunc fx(R0, KAPPA, THETA_V, SIGMA, PHI);
+	Doub root = rtnewt(fx, G, 1.0e-11);
+	cout << "The root value is: " << root;
+	return 0;
 }
